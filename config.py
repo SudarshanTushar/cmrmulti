783d043fadd097e9e1791a8806dfd9d8ab15d1ad
Tushar Gadhe <sudarshantushar04@gmail.com>
2026-01-10 11:40:45 +0530

    Update BOT_TOKEN in config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Telegram Bot Configuration
 API_ID = 24792582  # Replace with your api_id from https://my.telegram.org
 API_HASH = "d8fe9f718fd1ef56a8c4bfd2a9d37bda"  # Replace with your api_hash from https://my.telegram.org
-BOT_TOKEN = "7879880529:AAESIguOwcJCfNXbEVcyqFAxtFx456thP0I"  # Replace with your bot token from @BotFather
+BOT_TOKEN = "8070567824:AAEWSM21ROBNvWIOMa-mtQ9afqU22q6dZFI"  # Replace with your bot token from @BotFather
 
 # MongoDB Configuration
 MONGO_URI = "mongodb+srv://omkarwaghmare584:7XMZR6TqVjjlE3jL@cluster0.ucjntlp.mongodb.net/?retryWrites=true&w=majority"
